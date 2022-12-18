37c0267a1d8e814539e059ce7ebc33748895d5de
hbs30832 <hbs9312@gmail.com>
2022-12-18 16:21:18 +0900

    20221218
    - 투두리스트 스타일링 적용
    - 슬라이더 구현
    - 섹션 스크롤 애니메이션 구현

---
+++ practice-styling/src/App.js
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from "styled-components";
+import Section from "./components/Section";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  body {
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
+  }
+   
+  body::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Opera*/
+}
+`;
+
+function App() {
+  return (
+    <div>
+      <GlobalStyle />
+      <Section />
+    </div>
+  );
+}
+
+export default App;
